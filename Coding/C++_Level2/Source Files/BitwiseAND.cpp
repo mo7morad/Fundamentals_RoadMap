205c286cf420f8c515038eaee59728b000a87052
mo7morad <momurad.business@gmail.com>
2024-06-10 14:19:37 +0700

    new files added to c++_level2

---
+++ Coding/C++_Level2/Source Files/BitwiseAND.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Result: " << (12 & 25) << endl;
+  return 0;
+}
+
+/*
+
+00001100
+& (AND)
+00011001
+-----------
+00001000
+
+The result is 8 in Deciaml.
+
+simply (&) This operator is converting to the Bitwise and performe logical AND GATE sum operation.
+
+
+*/
---
+++ Coding/C++_Level2/Source Files/BitwiseOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Result: " << (12 | 25) << endl;
+    return 0;
+}
+
+/*
+00001100
+  (| OR)
+00011001
+-----------
+  00011101
+
+*/
