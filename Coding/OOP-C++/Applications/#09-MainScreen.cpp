5541bdc7fdd3eaf477cbd1cbb7ed9803d1599d59
Morad <momurad.business@gmail.com>
2024-12-27 14:44:23 +0700

    Total Balances added

---
+++ Coding/OOP-C++/Applications/#09-MainScreen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "clsMainScreen.h"
+
+int main()
+
+{
+  
+    clsMainScreen::ShowMainMenue();
+
+
+    system("pause>0");
+    return 0;
+}
--- Coding/OOP-C++/Applications/headers/clsBankClient.h
+++ Coding/OOP-C++/Applications/headers/clsBankClient.h
@@ -319,4 +319,16 @@ public:
     {
         return _LoadClientsDataFromFile();
     }
+
+    static float GetTotalBalances()
+    {
+        vector<clsBankClient> vClients = clsBankClient::GetClientsList();
+        double TotalBalances = 0;
+
+        for (clsBankClient Client : vClients)
+        {
+            TotalBalances += Client.GetAccountBalance();
+        }
+        return TotalBalances;
+    }
 };
