76106fe42f8c93f8d33f98a92a9462d70055496b
Morad <momurad.business@gmail.com>
2024-12-30 02:31:13 +0700

    Find Client Screen Added

--- Coding/OOP-C++/Applications/headers/clsMainScreen.h
+++ Coding/OOP-C++/Applications/headers/clsMainScreen.h
@@ -5,6 +5,7 @@
 #include "clsAddNewClientScreen.h"
 #include "clsDeleteClientScreen.h"
 #include "clsUpdateClientScreen.h"
+#include "clsFindClientScreen.h"
 #include "clsInputValidation.h"
 #include <iomanip>
 using namespace std;
@@ -57,7 +58,7 @@ private:
 
   static void _ShowFindClientScreen()
   {
-    cout << "\nFind Client Screen Will be here...\n";
+    clsFindClientScreen::ShowFindClientScreen();
   }
 
   static void _ShowTransactionsMenu()
