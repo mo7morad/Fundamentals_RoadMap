d55ef24ef0316d871d3385eaec93abb3a8b567d9
Morad <momurad.business@gmail.com>
2025-01-03 19:23:28 +0700

    adding the used user, and the date in the system screen

--- Coding/OOP-C++/Applications/headers/clsScreen.h
+++ Coding/OOP-C++/Applications/headers/clsScreen.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <iostream>
+#include "clsUser.h"
 #include "Global.h"
+#include "clsDate.h"
+
 using namespace std;
 
 class clsScreen
 {
-protected :
-    static void _DrawScreenHeader(string Title,string SubTitle ="")
+protected:
+    static void _DrawScreenHeader(string Title, string SubTitle = "")
     {
         cout << "\t\t\t\t\t______________________________________";
         cout << "\n\n\t\t\t\t\t  " << Title;
@@ -14,11 +17,14 @@ protected :
         {
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
-        cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\t______________________________________\n";
+        cout << "\n\t\t\t\t\tUser: " << CurrentUser.GetUserName() << "\n";
+        cout << "\t\t\t\t\tDate: " << clsDate::DateToString(clsDate()) << "\n\n";
     }
 
     static bool CheckAccessRights(clsUser::enPermissions Permission)
     {
+
         if (!CurrentUser.CheckAccessPermission(Permission))
         {
             cout << "\t\t\t\t\t______________________________________";
@@ -30,7 +36,5 @@ protected :
         {
             return true;
         }
-    return false;
     }
 };
-
