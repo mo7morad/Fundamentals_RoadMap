1fe511c0c8c4293de08fcf22e0164e3b7d4e8b75
Mohamed Morad <dronaessam@gmail.com>
2025-06-12 15:38:18 +0700

    adding new material

---
+++ Fundamentals/Coding/C# - Level 02/Custom Attributes Example/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class MyCustomAttribute : Attribute
+    {
+        public string Description { get; }
+
+        public MyCustomAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+
+    [MyCustom("This is a class attribute")]
+    class MyClass
+    {
+        [MyCustom("This is a method attribute")]
+        public void MyMethod()
+        {
+            // Method implementation
+        }
+    }
+
+    static void Main()
+    {
+        // Get the type of MyClass
+        Type type = typeof(MyClass);
+
+        // Get class-level attributes
+        object[] classAttributes = type.GetCustomAttributes(typeof(MyCustomAttribute), false);
+       
+        foreach (MyCustomAttribute attribute in classAttributes)
+        {
+            Console.WriteLine($"Class Attribute: {attribute.Description}");
+        }
+
+        // Get method-level attributes
+        MethodInfo methodInfo = type.GetMethod("MyMethod");
+        object[] methodAttributes = methodInfo.GetCustomAttributes(typeof(MyCustomAttribute), false);
+       
+        foreach (MyCustomAttribute attribute in methodAttributes)
+        {
+            Console.WriteLine($"Method Attribute: {attribute.Description}");
+        }
+
+        Console.ReadKey();
+
+    }
+}
