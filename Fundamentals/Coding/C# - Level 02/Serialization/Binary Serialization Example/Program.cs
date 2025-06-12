52735331a0741f7b4876fccaa1ab099891992cdc
Mohamed Morad <dronaessam@gmail.com>
2025-06-12 15:05:07 +0700

    Save local changes before pulling

--- Fundamentals/Coding/C# - Level 02/Serialization/Binary Serialization Example/Program.cs
+++ Fundamentals/Coding/C# - Level 02/Serialization/Binary Serialization Example/Program.cs
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -37,6 +38,46 @@ class Program
         }
     }
 }
+=======
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of the Person class
+        Person person = new Person { Name = "Mohammed Abu-Hadhoud", Age = 46 };
+
+
+        // Binary serialization
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream stream = new FileStream("person.bin", FileMode.Create))
+        {
+            formatter.Serialize(stream, person);
+        }
+
+
+        // Deserialize the object back
+        using (FileStream stream = new FileStream("person.bin", FileMode.Open))
+        {
+            Person deserializedPerson = (Person)formatter.Deserialize(stream);
+            Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+            Console.ReadKey();
+        }
+    }
+}
+>>>>>>> d9393c5e0564d28d3f69e74801c6f938d0dbce0e
 =======
 ﻿using System;
 using System.IO;
