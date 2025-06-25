6a39896f7878adbef55b7f2e183b68762473a991
Mohamed Morad <dronaessam@gmail.com>
2025-06-26 01:42:57 +0700

    Adding Cryptography in C#

---
+++ Fundamentals/Coding/C# - Level 02/String Builder Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int iterations = 200000;
+
+        // Concatenating strings using +
+        Stopwatch stopwatch1 = Stopwatch.StartNew();
+        ConcatenateStrings(iterations);
+        stopwatch1.Stop();
+        decimal x = stopwatch1.ElapsedMilliseconds / 1000;
+        Console.WriteLine($"String concatenation using + took: {x} ms");
+
+        // Concatenating strings using StringBuilder
+        Stopwatch stopwatch2 = Stopwatch.StartNew();
+        ConcatenateStringBuilder(iterations);
+        stopwatch2.Stop();
+        Console.WriteLine($"String concatenation using StringBuilder took: {stopwatch2.Elapsed} ms");
+    
+        Console.ReadKey();
+
+    }
+
+    static void ConcatenateStrings(int iterations)
+    {
+        string result = "";
+        for (int i = 0; i < iterations; i++)
+        {
+            result += "a";
+        }
+    }
+
+    static void ConcatenateStringBuilder(int iterations)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < iterations; i++)
+        {
+            sb.Append("a");
+            
+        }
+        string result = sb.ToString();
+    }
+}
