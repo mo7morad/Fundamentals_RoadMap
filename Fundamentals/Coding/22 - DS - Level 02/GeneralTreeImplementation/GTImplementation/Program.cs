1471c07618134fe9a7306a7d18a177575da72a90
Mohamed Morad <dronaessam@gmail.com>
2025-09-01 14:44:14 +0700

    C# some LINQ Lessons added

---
+++ Fundamentals/Coding/22 - DS - Level 02/GeneralTreeImplementation/GTImplementation/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneralTreeExample
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public List<TreeNode<T>> Children { get; set; }
+
+        public TreeNode(T value)
+        {
+            this.Value = value;
+            Children = new List<TreeNode<T>>();
+        }
+
+        public void AddChild(TreeNode<T> child)
+        {
+            Children.Add(child);
+        }
+    }
+
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+
+        public Tree(T rootValue)
+        {
+            Root = new TreeNode<T>(rootValue);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the tree
+            var CompanyTree = new Tree<string>("CEO");
+            var Finance = new TreeNode<string>("CFO");
+            var Tech = new TreeNode<string>("CTO");
+            var Marketing = new TreeNode<string>("CMO");
+
+            // Adding departments to the CEO node
+            CompanyTree.Root.AddChild(Finance);
+            CompanyTree.Root.AddChild(Tech);
+            CompanyTree.Root.AddChild(Marketing);
+
+            // Adding employees to departments
+            Finance.AddChild(new TreeNode<string>("Accountant"));
+            Tech.AddChild(new TreeNode<string>("Developer"));
+            Tech.AddChild(new TreeNode<string>("UX Designer"));
+            Marketing.AddChild(new TreeNode<string>("Social Media Manager"));
+
+            // Printing the tree
+            PrintTree(CompanyTree.Root);
+            Console.ReadKey();
+
+        }
+
+        public static void PrintTree<T>(TreeNode<T> node, string indent = " ")
+        {
+            Console.WriteLine(indent + node.Value);
+            foreach (var child in node.Children)
+            {
+                PrintTree(child, indent + "  ");
+            }
+        }
+    }
+}
