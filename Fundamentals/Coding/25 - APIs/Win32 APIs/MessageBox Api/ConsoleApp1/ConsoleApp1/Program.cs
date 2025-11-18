82546084b33a100d76f93e753d469e22151a6548
Morad <momurad.business@gmail.com>
2025-11-19 04:52:51 +0700

    adding msg box api

---
+++ Fundamentals/Coding/25 - APIs/Win32 APIs/MessageBox Api/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    // Import MessageBox function from user32.dll
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    static extern int MessageBox(IntPtr hWnd, String text, String caption, int type);
+
+    static void Main()
+    {
+        MessageBox(IntPtr.Zero, "Hello, World!", "My Message Box", 0);
+    }
+}
