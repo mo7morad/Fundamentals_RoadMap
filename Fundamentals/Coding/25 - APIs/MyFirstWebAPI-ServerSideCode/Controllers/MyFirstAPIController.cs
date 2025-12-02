9704344c6107ef336ec8c20d0e7c720dae2f1b37
Mohamed Morad <dronaessam@gmail.com>
2025-12-02 19:03:36 +0700

    Created my first web API !!

---
+++ Fundamentals/Coding/25 - APIs/MyFirstWebAPI-ServerSideCode/Controllers/MyFirstAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI_ServerSideCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyFirstAPIController : ControllerBase
+    {
+
+        [HttpGet("GetMyName", Name = "MyName")]
+        public String GetMyName()
+        {
+            return "My name is Mohamed Morad";
+        }
+
+        [HttpGet("GetYourName", Name = "YourName")]
+        public String GetYourName()
+        {
+            return "Your Name is Ali Basha";
+        }
+
+
+        [HttpGet("TwoNumsSumFunc")]
+        public int Sum2Numbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
