6628c57a6024b4537d31bb390a43076c6e71cf37
Morad <momurad.business@gmail.com>
2025-12-06 15:36:03 +0700

    Car moving Simulation

---
+++ Fundamentals/Coding/Problem-Solving-level-06-in-C#/30 Queue & Stack Problems/Vehicles Traffic Simulation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<Vehicle> vehiclesQueue = new Queue<Vehicle>();
+        vehiclesQueue.Enqueue(new Vehicle("M1", "BMW"));
+        vehiclesQueue.Enqueue(new Vehicle("Bike", "Halayoulya"));
+        vehiclesQueue.Enqueue(new Vehicle("Destroyer", "Jeap"));
+        vehiclesQueue.Enqueue(new Vehicle("LandCruser", "Coupee"));
+
+        VehiclesTraffic vehicles = new VehiclesTraffic(vehiclesQueue);
+        vehicles.Simulation();
+    }
+
+    public class VehiclesTraffic
+    {
+        private Queue<Vehicle> _vehicles;
+        public VehiclesTraffic(Queue<Vehicle> vehicles)
+        {
+            _vehicles = vehicles;
+        }
+
+        public void Simulation()
+        {
+            while (_vehicles.Count > 0)
+            {
+                Console.WriteLine($"\n[SIGNAL GREEN] {_vehicles.Dequeue()} has passed the signal.");
+                
+                if (_vehicles.Count > 0)
+                {
+                    Console.WriteLine("Vehicles waiting: " + string.Join(", ", _vehicles));
+                    
+                    Console.Write("Next vehicle approaching..."); 
+                    Thread.Sleep(1500); 
+                }
+                else
+                {
+                    Console.WriteLine("No vehicles waiting. Traffic is clear.");
+                }
+            }
+        }
+    }
+
+    public class Vehicle
+    {
+        private string _name;
+        private string _model;
+        public Vehicle(string Name, string Model)
+        {
+            _name = Name;
+            _model = Model;
+        }
+
+        public override string ToString()
+        {
+            return $"{_model} {_name}";
+        }
+    }
+}
